413f6bed22bff8279c78d48e3f36ea2621015c40
dev <dev@dev.com>
2020-05-24 13:24:37 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
-/**
- * Created by ChengYa on 2016/6/18.
- */
-
 //判断手机类型
 window.onload = function () {
     //alert($(window).height());
-    var u = navigator.userAgent;
-    if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {//安卓手机
-    } else if (u.indexOf('iPhone') > -1) {//苹果手机
+    let u = navigator.userAgent;
+    if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {
+        //安卓手机
+    } else if (u.indexOf('iPhone') > -1) {
+        //苹果手机
         //屏蔽ios下上下弹性
         $(window).on('scroll.elasticity', function (e) {
             e.preventDefault();
         }).on('touchmove.elasticity', function (e) {
             e.preventDefault();
         });
-    } else if (u.indexOf('Windows Phone') > -1) {//winphone手机
+    } else if (u.indexOf('Windows Phone') > -1) {
+        //win phone手机
     }
     //预加载
     loading();
 }
 
-var date_start;
-var date_end;
+let date_start;
+let date_end;
 date_start = getNowFormatDate();
 //加载图片
-var loading_img_url = [
+let loading_img_url = [
     "./image/0001.jpg",
     "./image/0002.jpg",
     "./image/0003.jpg",
@@ -33,23 +32,24 @@ var loading_img_url = [
     "./image/0006.jpg",
     "./image/0007.jpg",
     "./image/0008.jpg",
+    "./image/0009.jpg",
 ];
 
 //加载页面
 function loading() {
-    var numbers = 0;
-    var length = loading_img_url.length;
+    let numbers = 0;
+    let length = loading_img_url.length;
 
-    for (var i = 0; i < length; i++) {
-        var img = new Image();
+    for (let i = 0; i < length; i++) {
+        let img = new Image();
         img.src = loading_img_url[i];
         img.onerror = function () {
             numbers += (1 / length) * 100;
         }
+
         img.onload = function () {
             numbers += (1 / length) * 100;
             $('.number').html(parseInt(numbers) + "%");
-            console.log(numbers);
             if (Math.round(numbers) == 100) {
                 //$('.number').hide();
                 date_end = getNowFormatDate();
@@ -59,11 +59,16 @@ function loading() {
                     //拼接图片
                     $('.shade').hide();
                     var tagHtml = "";
-                    for (var i = 1; i <= 8; i++) {
+                    for (var i = 1; i <= 9; i++) {
                         if (i == 1) {
                             tagHtml += ' <div id="first" style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%"></div>';
-                        } else if (i == 41) {
+                        } else if (i == 9) {
                             tagHtml += ' <div id="end" style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%"></div>';
+                        } else if (i == 2){
+                            tagHtml += '' +
+                                ' <div id="page2-font" style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%">' +
+                                '<div id="page2Img" ></div>' +
+                                '</div>';
                         } else {
                             tagHtml += ' <div style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%"></div>';
                         }
@@ -99,6 +104,16 @@ function loading() {
                                 if (page == 1) {
                                     $(".btnImg").css("display", "none");
                                     $(".mark").css("display", "block");
+                                } else if (page == 2) {
+                                    $(".btnImg").css("display", "block");
+                                    $(".mark").css("display", "none");
+                                    let div = document.getElementById("page2-font");
+                                    div.classList.add("inOut");
+                                    $("#page2-font").addClass("inOut");
+
+                                    let page2Img = document.getElementById("page2Img");
+                                    page2Img.classList.add("font-class");
+                                    $("#page2Img").addClass("font-class");
                                 } else {
                                     $(".btnImg").css("display", "block");
                                     $(".mark").css("display", "none");
@@ -110,7 +125,6 @@ function loading() {
                                 }
                             },
                             turned: function (e, page, view) {
-                                console.log(page);
                                 var total = $(".flipbook").turn("pages");//总页数
                                 if (page == 1) {
                                     $(".return").css("display", "none");
